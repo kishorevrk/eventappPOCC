a0427b718cc45fdda26d9d330584d5a30aed3f7d
Dinesh B S <dineshbs444@gmail.com>
2021-10-11 19:03:10 +0530

    Config repo, added dependencies, added app structure and basic navigation!

---
+++ app/src/main/java/com/example/eventregistration/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.example.eventregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+
+        int SPLASH_TIME_OUT = 3500;
+        new android.os.Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }, SPLASH_TIME_OUT);
+    }
+}
